24e61e2dcf847cedd4bf9a28153c5afb4bd7153c
RyusukeMatsumoto <matsumotokakadevelop1102@gmail.com>
2020-09-20 11:19:06 +0900

    ブログサンプルのコードをコピーしてきた

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,89 @@
 import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
+import { Link, graphql } from "gatsby"
 
-export default function Home() {
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
+
+const BlogIndex = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  const posts = data.allMarkdownRemark.nodes
+
+  if (posts.length === 0) {
+    return (
+      <Layout location={location} title={siteTitle}>
+        <SEO title="All posts" />
+        <Bio />
+        <p>No blog posts found. Add markdown posts to "content/blog" (or the directory you specified for the "gatsby-source-filesystem" plugin in gatsby-config.js).</p>
+      </Layout>
+    )
+  }
+
   return (
-    <div style={{ color: `purple`}}>
-      <Link to="/contact/">Contact</Link>
-      <Header headerText="Hello Gatsby!"></Header>
-      <p>What a world.</p>
-      <img src="https://dic.nicovideo.jp/oekaki/861642.png" alt=""/>
-    </div>)
+    <Layout location={location} title={siteTitle}>
+      <SEO title="All posts" />
+      <Bio />
+      {posts.map((post) => {
+        const title = post.frontmatter.title || post.fields.slug
+        return (
+          <article
+            key={post.fields.slug}
+            itemScope
+            itemType="http://schema.org/Article"
+          >
+            <header>
+              <h3
+                style={{
+                  marginBottom: rhythm(1 / 4),
+                }}
+              >
+                <Link
+                  style={{ boxShadow: `none` }}
+                  to={post.fields.slug}
+                  itemProp="url"
+                >
+                  <span itemProp="headline">{title}</span>
+                </Link>
+              </h3>
+              <small>{post.frontmatter.date}</small>
+            </header>
+            <section>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: post.frontmatter.description || post.excerpt,
+                }}
+                itemProp="description"
+              />
+            </section>
+          </article>
+        )
+      })}
+    </Layout>
+  )
 }
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
+        }
+      }
+    }
+  }
+`
