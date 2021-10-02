f69c6e06a6a56d29e8abb464fd56e6c72357ad51
RyusukeMatsumoto <matsumotokakadevelop1102@gmail.com>
2021-10-02 12:38:51 +0900

    tagごとの一覧ページ予定地を作成
    元のコードは全部捨ててこの状態から始めることとした、もしかしたらかえって近道かもしれない

--- src/templates/tags-index.js
+++ src/templates/tags-index.js
@@ -1,82 +1,90 @@
-import React from "react";
+import React from "react"
+import { Link, graphql } from "gatsby"
 
-import Bio from "../components/bio";
-import Layout from "../components/seo";
-import SEO from "../components/seo";
-import { rhythm } from "../utils/typography";
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
 
-// Components.
-import { Link, graphql } from "gatsby";
+const TagIndexTamplate = ({ data, location }) => {
+  //const siteTitle = data.site.siteMetadata?.title || `Title`
+  const siteTitle = `Tag毎の一覧ページ作成予定地`
+  const posts = data.allMarkdownRemark.nodes
 
-
-const TagPageIndex = ({pageContext, data, location}) =>{
-  const {tag} = pageContext;
-  const {edges, totalCount} = data.allMarkdownRemark;
-
-  const tagHeader = `[${tag}]タグの記事一覧（全${totalCount}件）`;
+  if (posts.length === 0) {
+    return (
+      <Layout location={location} title={siteTitle}>
+        <SEO title="All posts" />
+        <Bio />
+        <p>No blog posts found. Add markdown posts to "content/blog" (or the directory you specified for the "gatsby-source-filesystem" plugin in gatsby-config.js).</p>
+      </Layout>
+    )
+  }
 
   return (
-    <Layout location={location}>
-      <SEO title={`Tag: ${tag}`} description={`${tag}タグを含む記事の一覧ページです`} />
+    <Layout location={location} title={siteTitle}>
+      <SEO title="All posts" />
       <Bio />
-      <h2>{tagHeader}</h2>
-      {edges.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug;
+      {posts.map((post) => {
+        const title = post.frontmatter.title || post.fields.slug
         return (
-          <article key={node.fields.slug}>
+          <article
+            key={post.fields.slug}
+            itemScope
+            itemType="http://schema.org/Article"
+          >
             <header>
               <h3
                 style={{
                   marginBottom: rhythm(1 / 4),
                 }}
               >
-                <Link style={{ boxShadow: `none` }} to={node.frontmatter.slug}>
-                  {title}
+                <Link
+                  style={{ boxShadow: `none` }}
+                  to={post.fields.slug}
+                  itemProp="url"
+                >
+                  <span itemProp="headline">{title}</span>
                 </Link>
               </h3>
-              <small>{node.frontmatter.date}</small>
+              <small>{post.frontmatter.date}</small>
             </header>
             <section>
               <p
                 dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
+                  __html: post.frontmatter.description || post.excerpt,
                 }}
+                itemProp="description"
               />
             </section>
           </article>
-        );
+        )
       })}
     </Layout>
-  );
-};
+  )
+}
 
-export default TagPageIndex;
+export default TagIndexTamplate
 
 export const pageQuery = graphql`
-  query($tag: String) {
+  query {
     site {
       siteMetadata {
         title
       }
     }
-    allMarkdownRemark(
-      limit: 2000
-      sort: { fields: [frontmatter___date], order: DESC }
-      filter: { frontmatter: { tags: { in: [$tag] } } }
-    ) {
-      totalCount
-      edges {
-        node {
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            date(formatString: "YYYY-MM-DD")
-            description
-          }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
         }
       }
     }
   }
-`;
+`
